73ba486ec0ea6841773d17b375deb0a704247c92
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-17 11:26:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
   index = input_to_index(input)
-  if valid_move(board, index)
+  if valid_move?(board, index)
     move(board, index, player = "X")
   else
     turn(board)
